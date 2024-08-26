6d0ce8ec3945f4dca9f34c334cb0282fb6d081f2
ValentinaMolinaL <isamoli.2026@gmail.com>
2024-08-26 10:41:47 -0500

    :construction:Dia en proceso

--- Dia5/scrip.js
+++ Dia5/scrip.js
@@ -1,5 +1,5 @@
 var data=[
-    {
+  {
     "products": [
       {
         "id": 1,
@@ -9,7 +9,9 @@ var data=[
         "quantityInStock": 50,
         "supplierId": 101
       }
-    ],
+    ]
+  },
+  {
     "suppliers": [
       {
         "id": 101,
@@ -20,7 +22,9 @@ var data=[
           "address": "123 Tech Lane, Silicon Valley, CA"
         }
       }
-    ],
+    ]
+  },
+  {
     "orders": [
       {
         "orderId": 1001,
@@ -32,21 +36,47 @@ var data=[
     ]
   }
 ]
+
 //Menu Principal
 var Opcion1=prompt("1).Gestion de productos\n2).Gestión de proveedores")//---------------------------fantan crud----------------------------------------
 if (Opcion1==1){
     //Gestion de productos
     var Opcion2=prompt("--------------------\nGestion de productos\n--------------------\n1).Crear\n2).Leer\n3).Actualizar\n4).Borrar")
-    //- **Crear**: Implementar una función `addProduct(product)` que añada un nuevo producto al inventario.
-    if (Opcion2==1){
-        function addProduct(){
-            var nueIdProd=prompt("Ingresa el ID del producto nuevo")
-            var nueNomProd=prompt("Ingresa el nombre del producto nuevo")
-            var nuecatProd=prompt("Ingresa la categoria del producto nuevo")
-            var nuePreProd=prompt("Ingresa el precio del producto nuevo")
-        }
+    if (Opcion2==1){//Crear: Implementar una función `addProduct(product)` que añada un nuevo producto al inventario.
+    var nueIdProd=prompt("Ingresa el ID del producto nuevo");
+    var nueNomProd=prompt("Ingresa el nombre del producto nuevo");
+    var nuecatProd=prompt("Ingresa la categoria del producto nuevo");
+    var nuePreProd=prompt("Ingresa el precio del producto nuevo");
+    var nueStocProd=prompt("ingresa la cantidad de stock que deseas crear");
+    var nueIdproveeProd=prompt("ingresa el ide del proveedor del producto");
+    function addProduct(){
+      prodNuevo={}
     }
-}
-//- **Leer**: Implementar una función `viewProducts()` que devuelva todos los productos del inventario.
+    prodNuevo=
+      {
+        "id": nueIdProd,
+          "name":nueNomProd,
+          "category":nuecatProd,
+          "price":nuePreProd,
+          "quantityInStock":nueStocProd,
+          "supplierId":nueIdproveeProd,
+        }
+    data[0]["products"].push(prodNuevo);
+    console.log(data)
+  }
+  /*
+  else if(Opcion2==2){//Leer: Implementar una función `viewProducts()` que devuelva todos los productos del inventario.
+    let i =0;
+    while (i == data.products.id.len){
+      let prodVer=prompt("ingresa el id del producto que deseas ver");
+      if(i == prodVer){
+        console.log( data.products[cont])
+      }
+      i++;
+    }
+  }
+  */
 //- Actualizar**: Implementar una función `updateProduct(id, newDetails)` que actualice los detalles de un producto por su ID.
-//- Borrar**: Implementa una función `deleteProduct(id)` que elimina un producto del inventario.
+
+}
+//- Borrar**: Implementa una función `deleteProduct(id)` que elimina un producto del inventario
