602623ac10de6a560f0987620e88ce0ce01ff02e
ValentinaMolinaL <isamoli.2026@gmail.com>
2024-09-02 09:36:07 -0500

    :sparkles:in process

--- Dia7/scrip.js
+++ Dia7/scrip.js
@@ -1,5 +1,76 @@
-document.getElementById(`button1`).addEventListener(`click`,getDataFromAPI);
-function getDataFromAPI(){
-    fetch("https://superheroapi.com/")
-    .then()
-}
+
+document.getElementById('enviar').addEventListener('click',function(){
+    const id=document.getElementById('idbusqueda').value;
+    fetch(`https://superheroapi.com/api.php/a17b7d1498b611a8a388687a1db1da5b/${id}`)
+    .then(Response => Response.json())
+    .then(Data => {
+        var nuevosdatos=`
+        <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">name</th>
+                    <th scope="col">powerstats</th>
+                    <th scope="col">biography</th>
+                    <th scope="col">appearance</th>
+                    <th scope="col">work</th>
+                    <th scope="col">connections</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="col">${Data.id}</th>
+                    <td scope="row">${Data.name}</td>
+                    <td scope="col">
+                    <p>intelligence ${Data.powerstats.intelligence}</p>
+                    <p>strength ${Data.powerstats.strength}</p>
+                    <p>speed ${Data.powerstats.speed}</p>
+                    <p>durability ${Data.powerstats.durability}</p>
+                    <p>power ${Data.powerstats.power}</p>
+                    <p>combat ${Data.powerstats.combat}</p>
+                    </td>
+                    <td scope="col">
+                    <p>${Data.biography["full-name"]}</p>
+                    <p>${Data.biography["alter-egos"]}</p>
+                    <p>${Data.biography["aliase"]}</p>
+                    <p>${Data.biography["place-of-birth"]}</p>
+                    <p>${Data.biography["first-appearance"]}</p>
+                    <p>${Data.biography["publisher"]}</p>
+                    <p>${Data.biography["alignment"]}</p>
+                    </td>
+                    
+                    <td scope="col">
+                    <p>${Data.appearance["gender"]}</p>
+                    <p>${Data.appearance["race"]}</p>
+                    <p>${Data.appearance["height"]}</p>
+                    <p>${Data.appearance["weight"]}</p>
+                    <p>${Data.appearance["eye-color"]}</p>
+                    <p>${Data.appearance["hair-color"]}</p>
+                    </td>
+                    <td scope="col">
+                    <p>${Data.work["occupation"]}</p>
+                    <p>${Data.work["base"]}<p>
+                    </td>
+                    <td scope="col">
+                    <p>${Data.connections["group-affiliation"]}</p>
+                    <p>${Data.connections["relatives"]}</p>
+                    </td>
+                    <td scope="col">
+                    <img src="${Data.image.url}" alt="">
+                    </td>
+
+                    
+
+
+
+                  </tr>
+                 
+                </tbody>
+              </table>
+        `
+        document.getElementById('datoss').innerHTML=nuevosdatos
+
+})
+
+
+})
---
+++ Dia8/scrip.js
@@ -0,0 +1,176 @@
+
+fetch("https://swapi.py4e.com/api/people/1")
+.then(resp => resp.json())
+.then(Data =>{
+    var estructura=`
+    <table class="table">
+  <thead>
+    <tr> 
+      <th scope="col">Name:</th>
+      <th scope="col">${Data.name}</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Height:</th>
+      <td>${Data.height}</td>
+    </tr>
+    <tr>
+      <th scope="row">Mass:</th>
+      <td>${Data.mass}</td>
+    </tr>
+    <tr>
+      <th scope="row">Hair_color:</th>
+      <td>${Data.hair_color}</td>
+    </tr>
+    <tr>
+      <th scope="row">skin_color:</th>
+      <td colspan="2">${Data.skin_color}</td>
+    </tr>
+    <tr>
+      <th scope="row">eye_color:</th>
+      <td colspan="2">${Data.eye_color}</td>
+    </tr>
+    <tr>
+      <th scope="row">birth_year:</th>
+      <td colspan="2">${Data.birth_year}</td>
+    </tr>
+   <tr>
+      <th scope="row">gender:</th>
+      <td colspan="2">${Data.gender}</td>
+    </tr>
+   <tr>
+      <th scope="row">homeworld:</th>
+      <td colspan="2">      
+      <div class="card text-bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-body" id="superMundo">
+        </div>
+      </div></td>
+    </tr>
+   <tr>
+      <th scope="row">films:</th>
+      <td colspan="2">
+      <div class="card text-bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+        </div>
+      </div>
+      </td>
+    </tr>
+  </tbody>
+</table>`
+
+var mundo =(Data.homeworld)
+fetch(mundo)
+.then(mun=> mun.json())
+.then (mundoT =>{
+  let GuardarMundo=document.getElementById("superMundo")
+  GuardarMundo.innerHTML=`
+     <table class="table">
+      <thead>
+        <tr> 
+          <th scope="col">Name:</th>
+          <th scope="col">${mundoT.name}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">rotation_period:</th>
+          <td>${mundoT.rotation_period}</td>
+        </tr>
+        <tr>
+          <th scope="row">orbital_period:</th>
+          <td>${mundoT.orbital_period}</td>
+        </tr>
+        <tr>
+          <th scope="row">diameter:</th>
+          <td>${mundoT.diameter}</td>
+        </tr>
+        <tr>
+          <th scope="row">climate:</th>
+          <td colspan="2">${mundoT.climate}</td>
+        </tr>
+        <tr>
+          <th scope="row">gravity:</th>
+          <td colspan="2">${mundoT.gravity}</td>
+        </tr>
+        <tr>
+          <th scope="row">terrain:</th>
+          <td colspan="2">${mundoT.terrain}</td>
+        </tr>
+        <tr>
+          <th scope="row">surface_water:</th>
+          <td colspan="2">${mundoT.surface_water}</td>
+        </tr>
+        <tr>
+          <th scope="row">population:</th>
+          <td colspan="2">${mundoT.population}</td>
+        </tr>
+        <tr>
+          <th scope="row">residents:</th>
+          <td colspan="2">${mundoT.residents}</td>
+        </tr>
+      </tbody>
+    </table>`
+})
+
+
+document.getElementById(`guardaStrucGen`).innerHTML=estructura  
+
+
+})
+
+var pel =(Data.films)
+fetch(peliculas)
+.then(pel=> pel.json())
+.then(peliculasT =>{
+  let guardarpelis=document.getElementById("mundos");
+  showfil.innerHTML+=`
+    <table class="table">
+      <thead>
+        <tr> 
+          <th scope="col">Name:</th>
+          <th scope="col">${Dat.name}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Height:</th>
+          <td>${Data.height}</td>
+        </tr>
+        <tr>
+          <th scope="row">Mass:</th>
+          <td>${Data.mass}</td>
+        </tr>
+        <tr>
+          <th scope="row">Hair_color:</th>
+          <td>${Data.hair_color}</td>
+        </tr>
+        <tr>
+          <th scope="row">skin_color:</th>
+          <td colspan="2">${Data.skin_color}</td>
+        </tr>
+        <tr>
+          <th scope="row">eye_color:</th>
+          <td colspan="2">${Data.eye_color}</td>
+        </tr>
+        <tr>
+          <th scope="row">birth_year:</th>
+          <td colspan="2">${Data.birth_year}</td>
+        </tr>
+        <tr>
+          <th scope="row">gender:</th>
+          <td colspan="2">${Data.gender}</td>
+        </tr>
+        <tr>
+          <th scope="row">homeworld:</th>
+          <td colspan="2">${Data.homeworld.name}</td>
+        </tr>
+        <tr>
+          <th scope="row">films:</th>
+          <td colspan="2">${Data.films}</td>
+        </tr>
+      </tbody>
+    </table>`
+document.getElementById(`guardaStrucMun`).innerHTML=estructura1  
+})
+});
