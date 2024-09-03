13756d00ff0ddbc20f7d5b9c0248bf3ecc33a5ed
ValentinaMolinaL <isamoli.2026@gmail.com>
2024-09-03 08:52:42 -0500

    :sparkles:falta detalles

--- Dia9/scrip.js
+++ Dia9/scrip.js
@@ -1,1 +1,35 @@
-var poke =`https://pokeapi.co/api/v2/pokemon/`
+function total(){
+    
+    document.getElementById("BotonInput").addEventListener("input", (e)=>{
+        let guarId=document.getElementById("BotonInput").value;
+    
+        let App = "https://pokeapi.co/api/v2/pokemon/"
+
+        fetch(App+guarId)
+        .then(pro => pro.json())
+        .then(Pokeapi=>{
+
+        let Nomb = document.getElementById("nombre");
+        Nomb.innerHTML = `${Pokeapi.idt} - <span>${Pokeapi.forms[0].name}</span>`;
+
+        let imgPoke = document.getElementById("imgPoke");
+        imgPoke.innerHTML = `
+        <audio autoplay src="${Pokeapi.cries.latest}">
+        </audio>
+        <img src="${Pokeapi.sprites.other.showdown.front_default}">`
+
+        let Id = 0
+
+        document.getElementById("prev").addEventListener("click", (e) => {
+            Id = Pokeapi.idt - 1
+            guarId = String(Id)
+        })
+        document.getElementById("next").addEventListener("click",(e)=>{
+            Id = Pokeapi.idt + 1
+            guarId = String(Id)
+        })
+        total()
+})
+})
+}
+total()
